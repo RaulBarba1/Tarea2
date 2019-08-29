7e9a7ed8cbb90e54ac75d72d445612988dbdfca4
raulbarba <luar_barba@hotmail.com>
2019-08-29 00:20:29 -0600

    for

---
+++ ForIn.js
@@ -0,0 +1,4 @@
+var list = { a: 7, b: 2, c: 3 };
+for (var i in list) {
+    console.log(i);
+}
---
+++ ForOf.js
@@ -0,0 +1,5 @@
+var lista = ["da", "b", "c"];
+for (var _i = 0, lista_1 = lista; _i < lista_1.length; _i++) {
+    var b = lista_1[_i];
+    console.log(b);
+}
