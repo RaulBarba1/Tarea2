7d005283df3a0f0cae4f4634c50a5318f631ba2e
raulbarba <luar_barba@hotmail.com>
2019-08-29 21:41:50 -0600

    destestruct

---
+++ destestruct.js
@@ -0,0 +1,3 @@
+var array = [1, 2, 3, 4];
+var x = array[0], y = array[1], rest = array.slice(2);
+console.log(x, y, rest);
---
+++ destestruct.ts
@@ -0,0 +1,4 @@
+var array = [1, 2, 3, 4];
+var [x, y, ...rest] = array;
+
+console.log(x, y, rest); 
