0846c7e7ed56a5709201e4923f17ec004c7b60e2
raulbarba <luar_barba@hotmail.com>
2019-08-29 21:39:18 -0600

    destarrays

---
+++ destarrays.js
@@ -0,0 +1,4 @@
+var array = [1, 2, 3];
+console.log(array[0]);
+var x = array[0], y = array[1], z = array[2];
+console.log(x);
---
+++ destarrays.ts
@@ -0,0 +1,7 @@
+var array = [1, 2, 3];
+
+console.log(array[0]);
+
+var [x, y, z] = array;
+
+console.log(x); 
