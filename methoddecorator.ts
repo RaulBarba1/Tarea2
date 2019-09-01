126570e96628f34ec6f05a597c199d9ed8324c2b
raulbarba <luar_barba@hotmail.com>
2019-09-01 14:18:26 -0600

    mas

---
+++ methoddecorator.ts
@@ -0,0 +1,16 @@
+function MethodDecorator(
+    target:	Object,	
+    propertyKey:	string,	
+    descriptor:	TypedPropertyDescriptor<any>				
+    )	{
+    console.log("MethodDecorator called	on:	",	target,	propertyKey, descriptor);
+ }
+class MethodDecoratorExample { @MethodDecorator	method(){
+
+} }
+
+MethodDecorator	called	on:	{ method: [Function] } method { value: [Function], 
+writable: true, 
+enumerable: true, 
+configurable: true
+}
