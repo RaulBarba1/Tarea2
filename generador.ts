657f235645837942dc3f056d520e78e4c2a3c512
raulbarba <luar_barba@hotmail.com>
2019-09-01 12:59:11 -0600

    generador

---
+++ generador.ts
@@ -0,0 +1,10 @@
+function*	logGenerator()	{	
+    console.log(yield);	
+    console.log(yield);	
+    console.log(yield); 
+var	gen	=	logGenerator();
+gen.next();
+gen.next('pretzel');
+gen.next('california');	
+gen.next('mayonnaise');
+}
