661001da6e7a6cdf61d2cecc38c7dc649fcf8ebe
raulbarba <luar_barba@hotmail.com>
2019-09-01 14:18:59 -0600

    actu

--- methoddecorator.ts
+++ methoddecorator.ts
@@ -1,8 +1,8 @@
 function MethodDecorator(
     target:	Object,	
-    propertyKey:	string,	
-    descriptor:	TypedPropertyDescriptor<any>				
-    )	{
+    propertyKey: string,	
+    descriptor: TypedPropertyDescriptor<any>				
+    ) {
     console.log("MethodDecorator called	on:	",	target,	propertyKey, descriptor);
  }
 class MethodDecoratorExample { @MethodDecorator	method(){
